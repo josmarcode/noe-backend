bb06ff15724cbe118456e9a2d3b573fe0a3bd4c0
Josmar Dominguez <josmarddominguez@gmail.com>
2026-01-21 19:22:05 -0400

    config: basic setting and DRF setup

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY  = os.getenv('SECRET_KEY')
+DEBUG       = os.getenv('DEBUG') == 'True'
